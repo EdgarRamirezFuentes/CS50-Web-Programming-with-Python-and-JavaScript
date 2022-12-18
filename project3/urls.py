3807c1ff86ea10d7c5e24cf9daff20e23fa96161
Edgar Ramírez <edgar.ramirez.fuentes.dev@gmail.com>
2022-12-18 16:29:19 -0600

    project3 init commit

--- project3/urls.py
+++ project3/urls.py
@@ -1,4 +1,4 @@
-"""commerce URL Configuration
+"""project3 URL Configuration
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/3.0/topics/http/urls/
@@ -13,12 +13,10 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.conf import settings
-from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 
 urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("", include("auctions.urls"))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    path('admin/', admin.site.urls),
+    path('', include('mail.urls'))
+]
